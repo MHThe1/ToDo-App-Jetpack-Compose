06ef6a77b7bb6aba95164aa6c6331aac27122340
MHThe1 <mehedihtanvir@gmail.com>
2025-03-18 03:59:07 +0600

    Task adding functionality added

--- app/src/main/java/com/kotlintut/todoapp/MainActivity.kt
+++ app/src/main/java/com/kotlintut/todoapp/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kotlintut.todoapp.ui.TaskListScreen
 import com.kotlintut.todoapp.ui.theme.ToDoAppTheme
@@ -30,6 +25,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ToDoAppTheme {
-//        Greeting("Android")
+        TaskListScreen()
     }
 }
--- app/src/main/java/com/kotlintut/todoapp/ui/TaskListScreen.kt
+++ app/src/main/java/com/kotlintut/todoapp/ui/TaskListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -20,11 +21,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kotlintut.todoapp.ui.components.TaskInputDialog
 import com.kotlintut.todoapp.ui.components.TaskItem
 import com.kotlintut.todoapp.viewmodels.TaskViewModel
 
@@ -34,10 +41,12 @@ fun TaskListScreen(
 ) {
     val tasks by taskViewModel.tasks.collectAsState()
 
+    var showDialog by remember { mutableStateOf(false) }
+
     Scaffold (
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {},
+                onClick = { showDialog = true },
                 content = {
                     Icon(Icons.Default.Add, contentDescription = "Add ToDo")
                 }
@@ -62,6 +71,18 @@ fun TaskListScreen(
         Column (
             modifier = Modifier.padding(paddingValues)
         ) {
+            if (tasks.isEmpty()){
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("You have not added any tasks yet!",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.error)
+                }
+            }
             LazyColumn {
                 items(tasks) { task ->
                     TaskItem(
@@ -72,5 +93,15 @@ fun TaskListScreen(
                 }
             }
         }
+    }
+
+    if(showDialog) {
+        TaskInputDialog(
+            onDismiss = {showDialog = false},
+            onConfirm = {
+                taskViewModel.addTask(it)
+                showDialog = false
+            }
+        )
     }
 }
